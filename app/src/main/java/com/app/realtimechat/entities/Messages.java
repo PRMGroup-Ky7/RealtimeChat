0e2894a7cea8c0f03a4d740dd30ab5f7e72e06c5
Lê Trung Sang <66933430+sanglthe140466@users.noreply.github.com>
2022-06-19 00:19:39 +0700

    Done for config project
    Done config about layout + firebase

---
+++ app/src/main/java/com/app/realtimechat/entities/Messages.java
@@ -0,0 +1,72 @@
+package com.app.realtimechat.entities;
+
+public class Messages {
+    private String from, message, type, to, messageID, time, date, name;
+
+    public Messages() {
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessageID() {
+        return messageID;
+    }
+
+    public void setMessageID(String messageID) {
+        this.messageID = messageID;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
