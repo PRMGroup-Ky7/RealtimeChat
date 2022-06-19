4da0c5b01bf2a19d1bec74969ff3305785ab6124
Lê Trung Sang <66933430+sanglthe140466@users.noreply.github.com>
2022-06-20 02:38:27 +0700

    done for private chat
    Done for private chat

--- app/src/main/java/com/app/realtimechat/utils/Constants.java
+++ app/src/main/java/com/app/realtimechat/utils/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String CHILD_USERS = "Users";
     public static final String CHILD_CONTACTS = "Contacts";
     public static final String CHILD_GROUPS = "Groups";
+    public static final String CHILD_MESSAGES = "Messages";
 }
