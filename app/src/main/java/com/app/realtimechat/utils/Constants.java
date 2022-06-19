7196e2a0861c797441c6c0d7721ee837e27cbc8f
Lê Trung Sang <66933430+sanglthe140466@users.noreply.github.com>
2022-06-20 00:09:52 +0700

    get list of group chat
    Display list of group chat

--- app/src/main/java/com/app/realtimechat/utils/Constants.java
+++ app/src/main/java/com/app/realtimechat/utils/Constants.java
@@ -3,4 +3,5 @@ package com.app.realtimechat.utils;
 public class Constants {
     public static final String CHILD_USERS = "Users";
     public static final String CHILD_CONTACTS = "Contacts";
+    public static final String CHILD_GROUPS = "Groups";
 }
