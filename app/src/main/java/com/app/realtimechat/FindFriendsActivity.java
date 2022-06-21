2427f2cc38ca566763dcc647c64cd3e09d25a55d
DuongQuan <duongvuvietquan2912@gmail.com>
2022-06-21 09:52:51 +0700

    Done for find friends activity (Currently display all user including current user)

---
+++ app/src/main/java/com/app/realtimechat/FindFriendsActivity.java
@@ -0,0 +1,14 @@
+package com.app.realtimechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends2);
+    }
+}
